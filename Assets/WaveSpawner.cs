77fa25f9a9d004b4ec1e592f92d90848a11c4034
kjellverschuren <kjellverschuren@Kjells-MacBook-Pro.local>
2017-04-20 14:37:48 -0300

    0.03
    - WaveSpawner added
    - Countdown timer added

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemyPrefab;
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+	private int waveNumber = 1;
+	public Transform SpawnPoint;
+	public float WaitForSeconds;
+	public Text WaveCountdownText;
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update ()
+	{
+		if (countdown <= 0f)
+		{
+			StartCoroutine(SpawnWave());
+			SpawnWave();
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+
+		WaveCountdownText.text = Mathf.Round(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave ()
+	{
+		for (int i = 0; i < waveNumber; i++)
+		{
+			SpawnEnemy();
+			yield return new WaitForSeconds(WaitForSeconds);
+		}
+
+
+		waveNumber++;
+	}
+
+	void SpawnEnemy ()
+	{
+		Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+	}
+}
